f23e88f0b1b9e0a0e725037a9dec32222cb88bc0
tusharJ-deloitte <101543675+tusharJ-deloitte@users.noreply.github.com>
2023-01-03 16:57:01 +0530

    [modified] settings.py (added) cors

--- GrapheneTest/settings.py
+++ GrapheneTest/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -129,10 +130,22 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000'
+# CORS_ORIGIN_WHITELIST = [
+#     'http://127.0.0.1:3000'
+# ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://www.test-cors.org",
+    "http://127.0.0.1:3000",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://www.test-cors.org',
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
